d1005bda9d96e425b88a9ceadac6b90f5a91250b
Arfat naik <arfatnaik800@gmail.com>
2025-06-17 20:21:19 +0530

    final build

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # SecretMessage
+
+works by encoding the entered string into base64 format and then giving you a link which can then be used to view the encrypted message.
+
+## workflow
+
+takes the input --> turns it into base64 using btoa function --> hide the section which is not required --> takes the input and then feeds the url into it --> encrypted message is then decrypted using atob function --> displays the secret message.
+
+
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// takes the encrypted message and decrypts it using atob(base64 decoding)
 const {hash}=window.location;
 decrypted=atob(hash.replace("#",''));
 
+// if there is decrypted text then disables the two sections which are not needed and shows the secret message
 if(decrypted){
     document.querySelector('.link').style.display="none";
     document.querySelector('.container').style.display="none";
@@ -9,6 +11,7 @@ if(decrypted){
     document.querySelector('h1').innerHTML=decrypted;
 }
 
+// selects the form and listens for submission then is it is not empty encrypts it and shows it in section two ehich can then be send to others
 document.querySelector('form').addEventListener('submit',e=>{
     e.preventDefault();
     
@@ -19,9 +22,9 @@ document.querySelector('form').addEventListener('submit',e=>{
     document.querySelector('.link').style.display="block";
     document.querySelector('.container').style.display="none";
     const linkInput=document.querySelector('#link-code');
-    linkInput.value=`${window.location}#${encrypted}`
-    linkInput.select()
+    linkInput.value=`${window.location}#${encrypted}`;
+    linkInput.select();
     }
     else
-    alert("please enter an message!!")
+    alert("please enter an message!!");
 })
