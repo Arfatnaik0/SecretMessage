939919115e8f18ce925aca87e3b16fa880a0dab9
Arfatnaik0 <arfatnaik05@gmail.com>
2025-06-17 14:03:20 +0000

    build1.0

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
+const {hash}=window.location;
+decrypted=atob(hash.replace("#",''));
+
+if(decrypted){
+    document.querySelector('.link').style.display="none";
+    document.querySelector('.container').style.display="none";
+    document.querySelector('.message').style.display="flex";
+
+    document.querySelector('h1').innerHTML=decrypted;
+}
+
 document.querySelector('form').addEventListener('submit',e=>{
     e.preventDefault();
     
     const input=document.querySelector('#inputText');
     if(input.value.trim() != ''){
     const encrypted=btoa(input.value);
-    console.log(encrypted);
     
     document.querySelector('.link').style.display="block";
-    document.querySelector('#link-code').value=encrypted;
+    document.querySelector('.container').style.display="none";
+    const linkInput=document.querySelector('#link-code');
+    linkInput.value=`${window.location}#${encrypted}`
+    linkInput.select()
     }
+    else
     alert("please enter an message!!")
 })
